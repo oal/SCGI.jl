f9f72921ddb47029df785b1adabdd4b78b7d2b0b
Olav Andreas Lindekleiv <olav@lindekleiv.com>
2013-08-15 19:41:01 +0200

    Reorganized some code.

--- scgi.jl
+++ scgi.jl
@@ -1,39 +1,36 @@
-type NetString
+type Request
 	socket::TcpSocket
 	size::Int
 	headers::Dict{String, String}
-	data::ASCIIString
 	content::ASCIIString
 
-	NetString(socket::TcpSocket) = new(socket, 0, Dict{String, String}(), "")
+	Request(socket::TcpSocket) = new(socket, 0, Dict{String, String}(), "")
 end
 
 function parserequest(sock::TcpSocket)
-	ns = NetString(sock)
-	readenv!(ns)
+	req = Request(sock)
+	readheaders!(req)
 
-	ns
+	read(req.socket, Char)
+
+	req.content = readbytes(req.socket, int(req.headers["CONTENT_LENGTH"]))
+	req
 end
 
-function readsize(ns::NetString)
-	ns.size = int(readuntil(ns.socket, ':')[1:end-1])
-	return ns.size
+function readsize!(req::Request)
+	req.size = int(readuntil(req.socket, ':')[1:end-1])
+	return req.size
 end
 
-function reads!(ns::NetString)
-	size = readsize(ns)
-	ns.data = readbytes(ns.socket, size)
-	return ns.data
-end
-
-function readenv!(ns::NetString)
-	headers = reads!(ns)
-	items = split(headers, "\00")[1:end-1]
+function readheaders!(req::Request)
+	size = readsize!(req)
+	header_data::ASCIIString = readbytes(req.socket, size)
+	items = split(header_data, "\00")[1:end-1]
 
 	i = 1
 	while i <= length(items)
-		println("$(items[i])\t$(items[i+1])")
-		ns.headers[items[i]] = items[i+1]
+		#println("$(items[i])\t$(items[i+1])")
+		req.headers[items[i]] = items[i+1]
 		i += 2
 	end
 end
@@ -48,17 +45,12 @@ function scgi(server, handler)
 	end
 end
 
-function handlefunc(ns::NetString)
-	read(ns.socket, Char)
-	ns.content = readbytes(ns.socket, int(ns.headers["CONTENT_LENGTH"]))
-	println(ns.data)
-	println("----")
-	println(ns.content)
-	println(ns.socket, "Status: 200 OK")
-	println(ns.socket, "Content-Type: text/html")
-	println(ns.socket, "")
-	println(ns.socket, "<h2>Hello, $(ns.headers["DOCUMENT_URI"][2:])</h2>")
-	close(ns.socket)
+function handlefunc(req::Request)
+	println(req.socket, "Status: 200 OK")
+	println(req.socket, "Content-Type: text/html")
+	println(req.socket, "")
+	println(req.socket, "<h2>Hello, $(req.headers["DOCUMENT_URI"][2:])</h2>")
+	close(req.socket)
 end
 
 scgi(listen(9006), handlefunc)
